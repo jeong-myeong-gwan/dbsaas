5f16b3866df889acabf3502e1cee841d21c54db2
jeongmg <neurocide@gmail.com>
2026-03-11 14:01:25 +0900

    eventRepository 오류 수정.

--- src/main/java/mj/dbsaas/repository/EventRepository.java
+++ src/main/java/mj/dbsaas/repository/EventRepository.java
@@ -4,15 +4,27 @@ import mj.dbsaas.model.AgentEvent;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class EventRepository {
 
     private final JdbcTemplate jdbc;
+    private ObjectMapper om = new ObjectMapper();
 
     public void save(AgentEvent ev){
-
+    	
+    	String paylogJson = "";
+    	try {
+    		paylogJson = om.writeValueAsString(ev.getPayload());
+    	}catch(JsonProcessingException e) {
+    		log.debug("JSON 변환 오류:" + e.getMessage()); 		
+    	}
+    	
         jdbc.update("insert into agent_event (tenant_id, instance_id,agent_id,ts, seq, event_type, payload) values (?,?,?,?,?,?,?)",
         ev.getTenantId(),
         ev.getInstanceId(),
@@ -20,7 +32,7 @@ public class EventRepository {
         ev.getTs(),
         ev.getSeq(),
         ev.getType(),
-        ev.getPayload().toString()
+        paylogJson
         );
     }
 
